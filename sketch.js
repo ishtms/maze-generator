185160b315af2aecb33b962569799553a0525d38
Ishtmeet Singh <ish.rissam@gmail.com>
2018-04-02 01:11:04 +0530

    the maze creator now works unless it is trapped between 3 neighbors

--- sketch.js
+++ sketch.js
@@ -1,18 +1,31 @@
 var cells = [];
 var w = 60;
-
+var cols = 10
+var current;
 function setup() {
-    createCanvas(601, 601);
-    for(var yAxis = 0; yAxis < 10; yAxis++) {
-        for(var xAxis = 0; xAxis < 10; xAxis++) {
-            cells.push(new Cell(xAxis * 60, yAxis * 60, w));
+    createCanvas(800, 800);
+    for(var yAxis = 0; yAxis < cols; yAxis++) {
+        for(var xAxis = 0; xAxis < cols; xAxis++) {
+            cells.push(new Cell(xAxis, yAxis, w));
         }
     }
+    current = cells[0];
 }
 
 function draw() {
+    frameRate(3)
     //background(230)
     for(var index = 0; index < cells.length; index++) {
         cells[index].draw();
+    }
+
+    current.isVisited = true;
+    console.log("NEIGHBOR " + current.checkNeighbors())
+    var next = current.checkNeighbors();
+
+    if(next) {
+        next.isVisited = true;
+        current = next
+        console.log(next)
     }
 }
