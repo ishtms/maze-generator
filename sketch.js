03877b4945479b45e3a9be9cbd8c955b79647645
Ishtmeet Singh <ish.rissam@gmail.com>
2018-04-02 00:01:21 +0530

    modified loops and conditionals

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
+var cells = [];
+var w = 60;
+
 function setup() {
-    createCanvas(window.innerWidth, window.innerHeight);
+    createCanvas(601, 601);
+    for(var yAxis = 0; yAxis < 10; yAxis++) {
+        for(var xAxis = 0; xAxis < 10; xAxis++) {
+            cells.push(new Cell(xAxis * 60, yAxis * 60, w));
+        }
+    }
 }
 
 function draw() {
-background(200);
+    //background(230)
+    for(var index = 0; index < cells.length; index++) {
+        cells[index].draw();
+    }
 }
